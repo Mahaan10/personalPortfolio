67b324d0aacda49a9be2c69d799740abc5889348
Mahan <reza.mahan10@gmail.com>
2025-02-27 21:16:05 +0330

    services page started...

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import "aos/dist/aos.css";
+import AOS from "aos";
+import { useEffect } from "react";
 
 function Services() {
-  return <div>Services</div>;
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      delay: 400,
+    });
+  }, []);
+
+  return (
+    <div className="font-ibm pt-[80px] pl-5" data-aos="fade-down">
+      <div className="flex flex-col gap-y-4 font-bold">
+        <p className="italic">My Service</p>
+        <p className="uppercase text-5xl">My Specialties</p>
+      </div>
+    </div>
+  );
 }
 
 export default Services;
